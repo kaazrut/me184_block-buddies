b86db8e52ef5503c96f60a8f7c4d72d595e076f7
rasp_pi <kaazrut@gmail.com>
2016-03-30 21:56:23 -0400

    adding led files

--- led_test.py
+++ led_test.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-
+import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23,GPIO.OUT)
 GPIO.setup(21,GPIO.OUT)
@@ -15,3 +15,6 @@ else:
 
 
 
+wait(10)
+GPIO.output(23,GPIO.LOW)
+GPIO.output(21,GPIO.HIGH)
