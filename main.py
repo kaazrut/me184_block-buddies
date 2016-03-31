263ce14e2a96fa02ea02ba89df5616ed1de1b87c
kaazrut <kaazrut@gmail.com>
2016-03-30 23:46:13 -0400

    integrating qr reader with LED test code

--- main.py
+++ main.py
@@ -4,8 +4,27 @@ Created on Wed Mar 30 20:34:02 2016
 
 @author: turzaak
 """
-
+import RPi.GPIO as GPIO
+import time
 import qr_reader
 
-qrdat = qr_reader.qrdat()
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23,GPIO.OUT)
+GPIO.setup(21,GPIO.OUT)
 
+qrcode = qr_reader.qrdat()
+
+ledcheck = 1
+
+testcolor = "blue"
+
+if testcolor == qrcode:
+    GPIO.output(23,GPIO.LOW)
+    GPIO.output(21,GPIO.HIGH)
+else:
+    GPIO.output(23,GPIO.HIGH)
+    GPIO.output(21,GPIO.LOW)
+    
+wait(10)
+GPIO.output(23,GPIO.LOW)
+GPIO.output(21,GPIO.HIGH)    
