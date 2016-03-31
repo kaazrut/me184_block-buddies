bb6fb80ed468e385cdbd75637622e92510f3b44a
rasp_pi <kaazrut@gmail.com>
2016-03-30 21:58:34 -0400

    stupid git

--- ledtester.py
+++ ledtester.py
@@ -6,12 +6,17 @@ Created on Wed Mar 30 20:52:25 2016
 """
 
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-LED = 20
+LED = 23
 
 GPIO.setup(LED, GPIO.OUT)
 
 GPIO.output(LED, True)
+
+time.sleep(4)
+
+GPIO.output(LED, False)
