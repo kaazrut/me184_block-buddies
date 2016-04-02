ffc514d6604a22fb96a87bbc83e15e402cda2c01
kaazrut <kaazrut@gmail.com>
2016-04-02 01:30:08 -0400

    adding in audio and pseudocode structure

--- main.py
+++ main.py
@@ -7,12 +7,43 @@ Created on Wed Mar 30 20:34:02 2016
 import RPi.GPIO as GPIO
 import time
 import qr_reader
+import os
 
+#initialize I/Os
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23,GPIO.OUT)
 GPIO.setup(21,GPIO.OUT)
 
+#create variables and lists
+colorSelect = ["blue", "yellow", "orange", "purple"]
+colorList = []
+scannedList = []
+
+#at startup, run greeting
+os.system('mpg123 -q .\audio\mp3\IntroHello.mp3 &')
+wait(3)
+
+#Throw everything into a while loop
+
+os.system('mpg123 -q .\audio\mp3\NeedHelp.mp3 &')
+wait(1)
+
+#insert the three random colors
+#TODO figure out how to randomize that list
+os.system('mpg123 -q .\audio\mp3\blue.mp3 &')
+wait(1)
+os.system('mpg123 -q .\audio\mp3\yellow.mp3 &')
+wait(1)
+os.system('mpg123 -q .\audio\mp3\yellow.mp3 &')
+wait(3)
+
+#finish list
+os.system('mpg123 -q .\audio\mp3\CanOrderBlocks.mp3 &')
+
+
+#start checking for qr codes and then append them to a list
 qrcode = qr_reader.qrdat()
+scannedList.append(qrcode)
 
 ledcheck = 1
 
@@ -29,3 +60,4 @@ else:
 wait(10)
 GPIO.output(23,GPIO.LOW)
 GPIO.output(21,GPIO.HIGH)    
+
