8d8188e70a67a01a0cc5e82e41f075eeee5e773a
kaazrut <kaazrut@gmail.com>
2016-04-02 19:44:23 -0400

    ash discovered pep 8

--- main.py
+++ main.py
@@ -36,46 +36,76 @@ greenPWM.start(0)
 bluePWM.start(0)
 
 #create variables and lists
-colorSelect = ("blue", "yellow", "orange", "purple")
+colorSelect = ('blue', 'yellow', 'orange', 'purple')
 colorList = random.sample(colorSelect, 3)
 scannedList = []
 
-colorKey = {'blue': {'mp3': 'blue.mp3', 'valR': 0, 'rPul': 0, 'valG': 0, \
-            'gPul': 0, 'valB': 1, 'bPul': 100},
-            'yellow': {'mp3': 'yellow.mp3', 'valR': 1, 'rPul': 100,'valG': 1, \
-            'gPul': 100, 'valB': 0, 'bPul': 0},
-            'orange': {'mp3': 'orange.mp3', 'valR': 1, 'rPul': 100, 'valG': 1, \
-            'gPul': 50, 'valB': 0, 'bPul': 0},
-            'purple': {'mp3': 'purple.mp3', 'valR': 1, 'rPul': 100, 'valG': 0, \
-            'gPul': 0, 'valB': 1, 'bPul': 100}
-            }
+#for led color, 0 = on, 1 = off (this is due to wiring for GPIO)
+colorKey = {
+    'blue': {
+        'mp3': 'blue.mp3',
+        'valR': 1,
+        'rPul': 0,
+        'valG': 1,
+        'gPul': 0,
+        'valB': 0,
+        'bPul': 100
+    },
+    'yellow': {
+        'mp3': 'yellow.mp3',
+        'valR': 0,
+        'rPul': 100,
+        'valG': 0,
+        'gPul': 100,
+        'valB': 1,
+        'bPul': 0
+    },
+    'orange': {
+        'mp3': 'orange.mp3',
+        'valR': 0,
+        'rPul': 100,
+        'valG': 0,
+        'gPul': 50,
+        'valB': 1,
+        'bPul': 0
+    },
+     'purple': {
+        'mp3': 'purple.mp3',
+        'valR': 0,
+        'rPul': 100,
+        'valG': 1,
+        'gPul': 0,
+        'valB': 0,
+        'bPul': 100
+    }
+}
 
 #at startup, run greeting
-SOUND_PATH = os.path.join("audio", "mp3")
+SOUND_PATH = os.path.join('audio', 'mp3')
 os.system('mpg123 -q ' + os.path.join(SOUND_PATH, 'IntroHello.mp3 &'))
 time.sleep(10)
 
-#Throw everything into a while loop
+#Throw everything into a while loop from this point
 
 os.system('mpg123 -q ' + os.path.join(SOUND_PATH, 'NeedHelp.mp3 &'))
 time.sleep(5)
 
 #in loop, for each color selected, light the led and say the color
 for x in colorList:
-   mp3Sel = colorKey[x]['mp3']
-   cmd = 'mpg123 -q ' + os.path.join(SOUND_PATH, mp3Sel)
-   redPWM.ChangeDutyCycle(colorKey[x]['rPul'])
-   GPIO.output(redLED, colorKey[x]['valR'])
-   greenPWM.ChangeDutyCycle(colorKey[x]['gPul'])
-   GPIO.output(greenLED, colorKey[x]['valG'])
-   bluePWM.ChangeDutyCycle(colorKey[x]['bPul'])
-   GPIO.output(blueLED, colorKey[x]['valB']) 
-   os.system(cmd)
-   time.sleep(5)
-   GPIO.output(redLED, 1)
-   GPIO.output(greenLED, 1)
-   GPIO.output(blueLED, 1)
-   time.sleep(2)
+    mp3Sel = colorKey[x]['mp3']
+    cmd = 'mpg123 -q ' + os.path.join(SOUND_PATH, mp3Sel)
+    redPWM.ChangeDutyCycle(colorKey[x]['rPul'])
+    GPIO.output(redLED, colorKey[x]['valR'])
+    greenPWM.ChangeDutyCycle(colorKey[x]['gPul'])
+    GPIO.output(greenLED, colorKey[x]['valG'])
+    bluePWM.ChangeDutyCycle(colorKey[x]['bPul'])
+    GPIO.output(blueLED, colorKey[x]['valB']) 
+    os.system(cmd)
+    time.sleep(5)
+    GPIO.output(redLED, 1)
+    GPIO.output(greenLED, 1)
+    GPIO.output(blueLED, 1)
+    time.sleep(2)
    
 
 #finish list
