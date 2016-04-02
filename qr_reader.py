7c1e36bbd5e5dc4516be483c8c34c2ed870e6423
kaazrut <kaazrut@gmail.com>
2016-04-02 01:11:50 -0400

    we are trying to add a loop to continuously read for qr codes

--- qr_reader.py
+++ qr_reader.py
@@ -13,42 +13,47 @@ from PIL import Image
 import zbar
 
 def qrdat():
-    #create stream in memory
-    stream = io.BytesIO()
-    with picamera.PiCamera() as camera:
-        camera.start_preview()
-        time.sleep(2)
-        camera.capture(stream, format='jpeg')
+    while True:
+        symbol = None        
+        #create stream in memory
+        stream = io.BytesIO()
+        with picamera.PiCamera() as camera:
+            camera.start_preview()
+            time.sleep(2)
+            camera.capture(stream, format='jpeg')
+            
+        #read content from stream
+        stream.seek(0)
+        pil = Image.open(stream)
         
-    #read content from stream
-    stream.seek(0)
-    pil = Image.open(stream)
-    
-    #now create a reader for the stream
-    scanner = zbar.Proessor()
-    
-    #configure the reader
-    scanner.parse_config('enable')
-    
-    pil = pil.convert('L')
-    width, height = pil.size
-    raw = pil.tostring()
-    
-    #image data wrap
-    image = zbar.Image(width, height, 'Y800', raw)
-    
-    # scan the image for barcodes
-    scanner.scan(image)
-    
-    # extract results
-    for symbol in image:
-        os.system('mpg123 -q .\audio\mp3\Electronic_Chime-KevanGC.mp3 &')
-        print('decoded', symbol.type, 'symbol', '"%s"' % symbol.data)
-    
-    # clean up
-    del(image)    
-
-    return symbol.data
+        #now create a reader for the stream
+        scanner = zbar.ImageScanner()
+        
+        #configure the reader
+        scanner.parse_config('enable')
+        
+        pil = pil.convert('L')
+        width, height = pil.size
+        raw = pil.tostring()
+        
+        #image data wrap
+        image = zbar.Image(width, height, 'Y800', raw)
+        
+        # scan the image for barcodes
+        scanner.scan(image)
+           
+        #if find a barcode   
+        # extract results
+        for symbol in image:
+            os.system('mpg123 -q .\audio\mp3\Electronic_Chime-KevanGC.mp3 &')
+            print('decoded', symbol.type, 'symbol', '"%s"' % symbol.data)
+        
+        # clean up
+        del(image)    
+        
+        if symbol:
+            return symbol.data
+    #if do not find a barcode run again
     
 if __name__ == "__main__":
     print(qrdat())
