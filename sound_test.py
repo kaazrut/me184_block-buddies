860703798496cd8ea8e45acae0c55462e45a6cea
rasp_pi <kaazrut@gmail.com>
2016-04-11 22:25:25 -0400

    Sound tests.

--- sound_test.py
+++ sound_test.py
@@ -1,15 +1,28 @@
 import time
 import pygame
 import os
+import random
 
-SOUND_PATH = os.path.join('audio', 'mp3')
+SOUND_PATH = os.path.join('audio', 'wav')
+pygame.init()
 pygame.mixer.init()
 
-sonic = pygame.mixer.music.load('Sonic_the_Hedgehog_3_IceCapped_OC_ReMix.mp3')
-intro = pygame.mixer.music.load(os.path.join(SOUND_PATH, 'Intro.mp3'))
-needHelp = pygame.mixer.music.load(os.path.join(SOUND_PATH, 'NeedHelp.mp3'))
+#pygame.mixer.music.load('Sonic_the_Hedgehog_3_IceCapped_OC_ReMix.mp3')
+intro = pygame.mixer.Sound(os.path.join(SOUND_PATH, 'blue.wav'))
+needHelp = pygame.mixer.Sound(os.path.join(SOUND_PATH, 'yellow.wav'))
 
-pygame.mixer.music.play(0, 0.0)
-sonic.play()
-intro.queue()
-needHelp.queue()
+#pygame.mixer.music.play(0, 0.0)
+channel = pygame.mixer.find_channel()
+channel.queue(intro)
+#channel.queue(needHelp)
+#channel.queue(intro)
+
+#while channel.get_queue() or channel.get_busy():
+#    time.sleep(0.1)
+
+while True:
+    if not channel.get_queue():
+        channel.queue(random.choice((intro, needHelp)))
+
+#intro.queue()
+#needHelp.queue()
